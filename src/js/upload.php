593b144ff0792fd1b47fe16a2e7aac0694b7bcc7
pgexec <paulo.gomesjr123@gmail.com>
2024-02-22 16:21:52 -0300

    second

--- src/js/upload.php
+++ src/js/upload.php
@@ -1,11 +1,27 @@
-<?php
+<!-- <?php
 
-header('Acess-Control-Allow-Origin:http://127.0.0.1:5501/ ');
+// header('Acess-Control-Allow-Origin:http://127.0.0.1:5501/ ');
 
 
-$tmp = $_FILES['file']['tmp_name'];
-$extension = $_POST['extension'];
+// $tmp = $_FILES['file']['tmp_name'];
+// $extension = $_POST['extension'];
 
-$acceptedExtensions = ['jpg','pgn','jpeg'];
+// $acceptedExtensions = ['jpg','pgn','jpeg'];
 
-if(!in_array());
+// if(!in_array($extension,$acceptedExtensions))
+// {
+
+//     http_response_code(415);
+//     echo json_encode('extension_error');
+//     return;
+// }
+
+// if(move_uploaded_file($tmp, './uploads/' .uniqid().'.'. $extension))
+// {
+//     http_response_code(200);
+//     echo json_encode('Uploaded');
+//     return;
+// }
+
+// http_response_code(404);
+// echo json_encode('not_uploaded'); -->
